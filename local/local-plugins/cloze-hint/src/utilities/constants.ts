7b85dee2a3a4621c7412454fd12eb922614c349c
Delta One <39011780+d3lta0ne@users.noreply.github.com>
2023-05-07 01:58:41 -0400

    added file structure

---
+++ local/local-plugins/cloze-hint/src/utilities/constants.ts
@@ -0,0 +1,5 @@
+export const clozeHintsPowerupCode = "closeHints"
+export const hintsSlotCode = "hints"
+export const hintsStorageKey = "hintsKey"
+export const taggedRemStorageKey = 'taggedRemStorageKey'
+export const clozePropsStorageKey = 'inputProps'
